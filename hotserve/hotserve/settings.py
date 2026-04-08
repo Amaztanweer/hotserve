2716302e02d85083059bf51524cc92e1ab1f2c58
Md Amaz Tanweer <amaztanweer6@gmail.com>
2026-04-08 19:27:37 +0530

    Update settings.py

--- hotserve/hotserve/settings.py
+++ hotserve/hotserve/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ─────────────────────────────────────────────
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-this-in-production-hotserve-2024')
 DEBUG=False
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['hotserve.onrender.com', '127.0.0.1', 'localhost']
 
 # ─────────────────────────────────────────────
 # APPLICATIONS
